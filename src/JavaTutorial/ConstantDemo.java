543afc729e5d4d3749e00e4843a16379c4d99a34
dtc16 <dtc16@DESKTOP-7SHSBFQ>
2020-11-19 02:22:45 +0900

    2020.11.19

---
+++ src/JavaTutorial/ConstantDemo.java
@@ -0,0 +1,27 @@
+package JavaTutorial;
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+		float a = 2.2F;
+		long b = 2147383648L;
+		byte c = 100;
+		short d = 200;
+		
+	
+	}
+
+	/* 상수 뒤에 F 붙이기
+	 * : float형이라는 것을 명시적으로 표시
+	 * 
+	 * 실수인 상수의 기본 데이터 형
+	 * : double
+	 * 
+	 * 상수 뒤에 L 붙이기
+	 * : long형이라는 것을 명시적으로 표시
+	 * 
+	 * byte나 short 형인 경우
+	 * : 형변환을 안해줘도 자동으로 변환 가능!!
+	 */
+}
+
---
+++ src/JavaTutorial/DivisionDemo.java
@@ -0,0 +1,18 @@
+package JavaTutorial;
+
+public class DivisionDemo {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		
+		float c = 10.0F;
+		float d = 3.0F;
+		
+		System.out.println(a/b);
+		System.out.println(c/d);
+		System.out.println(a/d); // 정수 a를 실수형으로 형변환이 되어 실수값이 나옴..
+
+	}
+
+}
